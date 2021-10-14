f9efec3ed16f0a5e5148ed40b0c09ad11f0015db
Gábor Tamás Tornai <gabo0127@edu.easj.dk>
2021-10-14 14:18:21 +0200

    Add project files.

---
+++ ClassLibrary/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Book
+    {
+
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+        public string ISBN { get; set; }
+
+
+
+
+    }
+}
